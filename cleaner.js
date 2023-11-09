5ca213d2f4499350fab961e8ff5842cd3cde4a45
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2023-11-09 13:24:47 +0400

    init cleaner.js

---
+++ cleaner.js
@@ -0,0 +1,29 @@
+const DELAY = 2_000;
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+async function deleteConversations() {
+    let conversations = document.getElementsByClassName('flex p-3 items-center gap-3');
+
+    while (conversations.length > 0) {
+        conversations[0].click();
+        await delay(DELAY);
+
+        const deleteIcons = document.getElementsByClassName('p-1 hover:text-token-text-primary');
+        if (deleteIcons[1]) {
+            deleteIcons[1].click();
+            await delay(DELAY);
+
+            const deleteButtons = document.getElementsByClassName('btn relative btn-danger');
+            if (deleteButtons[0]) {
+                deleteButtons[0].click();
+                await delay(DELAY);
+            }
+        }
+        conversations = document.getElementsByClassName('flex p-3 items-center gap-3');
+    }
+}
+
+deleteConversations();
